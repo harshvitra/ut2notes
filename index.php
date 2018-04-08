57e043ccdf08cbe02e5c47155afd63d526e76a5e
Harsh Vitra <jainharsh44@gmail.com>
2018-04-09 00:40:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,71 @@
-<?php 
-	require("snmr.html");
-?>
+
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>UT notes</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="shortcut icon" href="img/logo.png">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <style>
+            body {
+                padding-top: 0px;
+                padding-bottom: 0px;
+                background-color: #ecf0f1;
+            }
+        </style>
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <!--[if lt IE 9]>
+            <script src="js/vendor/html5-3.6-respond-1.4.2.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    
+    <div class="container">
+      <div class="header" style="width: 75%;margin: 10px auto;padding: 10px;">
+       <center>
+         
+       </center> 
+      </div>
+    </div>
+   
+   <div class="row" style="width: 85%;margin: 0px auto;">
+     <div class="col col-sm-4" style="padding: 20px;">
+       <center>
+         <a href="snmr.html">
+          <div style="width: 80%;height: auto;background-color:white; padding: 30px; ">
+            <h4 style="color:#2c3e50; font-family: 'Roboto', sans-serif;">SNMR</h4>
+            
+          </div>
+         
+          </a>
+       </center>
+     </div>
+     <div class="col col-sm-4" style="padding: 20px;">
+       <center>
+        <a href="csm.mht">
+         <div style="width: 80%;height:auto;background-color:white; padding: 30px; ">
+          <h4 style="color:#2c3e50; font-family: 'Roboto', sans-serif;">CSM</h4>
+          
+         </div>
+       </a>
+       </center>
+     </div>
+    
+
+      <footer>
+       
+      </footer>
+    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
